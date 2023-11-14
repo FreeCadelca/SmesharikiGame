2d86f54d40ebb8f7fe45e1924fb7bcc348a53cfa
FreeCadelca <746503645272548akk@gmail.com>
2023-11-15 01:39:31 +0300

    reworked system of sections in menu integrated into the code menu: 0.1a4

--- menu.py
+++ menu.py
@@ -55,11 +55,16 @@ class MainMenuSection(AbstractSection):
         super().input(keys, last_pressed_keys, current_section)
         if keys[pygame.K_RETURN] and not last_pressed_keys[pygame.K_RETURN]:
             current_section = self.current_bar
+            # print(current_section)
             last_pressed_keys[pygame.K_RETURN] = True
         for i in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_RETURN):
             # refreshing dict of last pressed keys
             if not keys[i]:
                 last_pressed_keys[i] = False
+        return current_section
+        # we cannot rewrite int attribute of class Menu (self.current_section) bcs it mutable,
+        # so I return a new self.current_section value and assign it to the original each time,
+        # then check if something has changed
 
     def setup_bars(self, bars_sprites):
         super().setup_bars(bars_sprites)
@@ -87,13 +92,11 @@ class MainMenuSection(AbstractSection):
 
 
 class Menu:
-    BARS_MAIN_MENU = ['Play', 'Account', 'Settings', 'About ass']
+    SECTIONS = [MainMenuSection]
 
     def __init__(self, surface):
         self.bars_sprites = None
         self.surface = surface
-        self.current_bar = 0
-        self.max_bar = 4
         self.current_section = 0
         self.last_pressed_keys = {
             pygame.K_UP: False,
@@ -102,86 +105,18 @@ class Menu:
             pygame.K_RIGHT: False,
             pygame.K_RETURN: False
         }
-
-    def input_main_menu(self, keys):
-        if keys[pygame.K_RETURN] and not self.last_pressed_keys[pygame.K_RETURN]:
-            self.current_section = self.current_bar
-            self.last_pressed_keys[pygame.K_RETURN] = True
-            return 0
-
-    def input_account(self, keys):
-        if keys[pygame.K_ESCAPE]:
-            self.current_section = 0
-            self.current_bar = 0
-            self.max_bar = 4
-
-    def input_settings(self, keys):
-        pass
-
-    def input_about_ass(self, keys):
-        pass
+        self.section = MainMenuSection()
 
     def input(self):
-        inputs = [self.input_main_menu,
-                  self.input_account,
-                  self.input_settings,
-                  self.input_about_ass]
-        keys = pygame.key.get_pressed()
-        # Two of actions can be written the same for all sections - it is a moving of current bar.
-        # After that remaining keys should be written separately for each section
-        if keys[pygame.K_DOWN] and not self.last_pressed_keys[pygame.K_DOWN]:
-            self.current_bar = (self.current_bar + 1) % self.max_bar
-            self.last_pressed_keys[pygame.K_DOWN] = True
-            return 0
-        elif keys[pygame.K_UP] and not self.last_pressed_keys[pygame.K_UP]:
-            self.current_bar = (self.current_bar - 1) % self.max_bar
-            self.last_pressed_keys[pygame.K_UP] = True
-            return 0
-        inputs[self.current_section]()
-
-        for i in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT, pygame.K_RETURN):
-            # refreshing dict of last pressed keys
-            if not keys[i]:
-                self.last_pressed_keys[i] = False
-
-    def setup_bars_main_menu(self):
-        self.bars_sprites = pygame.sprite.Group()
-        for i in range(len(Menu.BARS_MAIN_MENU)):
-            offset = ((i - len(Menu.BARS_MAIN_MENU) // 2) * DefaultBar.BAR_HEIGHT +  # offset relative other bars
-                      20 * (i - len(Menu.BARS_MAIN_MENU) // 2))  # spacing
-            new_bar = DefaultBar(
-                Menu.BARS_MAIN_MENU[i],
-                (screen_width // 2, screen_height // 2 + offset),
-                'menu\\' + Menu.BARS_MAIN_MENU[i] + '.png')
-            self.bars_sprites.add(new_bar)
-
-        offset_of_current_bar = ((self.current_bar - len(Menu.BARS_MAIN_MENU) // 2) * DefaultBar.BAR_HEIGHT +
-                                 20 * (self.current_bar - len(Menu.BARS_MAIN_MENU) // 2))
-        current_bar_sprite = DefaultBar(
-            'selected',
-            (screen_width // 2, screen_height // 2 + offset_of_current_bar),
-            'menu\\Stroke.png'
-        )
-        self.bars_sprites.add(current_bar_sprite)
-
-        # for i in self.bars_sprites:
-        #     print(i.rect.center, end='; ')
-
-    def setup_bars_account(self):
-        pass
-
-    def setup_bars_settings(self):
-        pass
-
-    def setup_bars_about_ass(self):
-        pass
+        previous_section = self.current_section
+        self.current_section = self.section.input(pygame.key.get_pressed(), self.last_pressed_keys, self.current_section)
+        if previous_section != self.current_section:
+            print(previous_section, self.current_section)
+            self.section = Menu.SECTIONS[self.current_section]()
 
     def setup_bars(self):
-        setups = [self.setup_bars_main_menu,
-                  self.setup_bars_account,
-                  self.setup_bars_settings,
-                  self.setup_bars_about_ass]
-        setups[self.current_section]()
+        self.bars_sprites = pygame.sprite.Group()
+        self.section.setup_bars(self.bars_sprites)
 
     def run(self):
         self.setup_bars()

614cda6e69c290f5d35d6a70be232993f12570c4
Sssofiko <Berdussofia@mail.ru>
2023-11-16 16:59:39 +0300

    Intro

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import pygame
+import pygame, sys
+from settings import *
+import cv2
 import moviepy.editor
 
-pygame.init()
-video = moviepy.editor.VideoFileClip("source\\preview.mp4")
-video.preview()
-pygame.quit()
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode([screen_width, screen_height])
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+game = Game()
+video = moviepy.editor.VideoFileClip("../SmesharikiGame/source/preview.mp4")
+intro = video.resize((screen_width, screen_height))
+intro.preview()

e13625b9fb4dfadff62bee55365db579ae239071
FreeCadelca <746503645272548akk@gmail.com>
2023-12-12 18:10:38 +0300

    fixed create_path_on_platform.py (deleted replacing of '.' to '..'

--- create_path_on_platform.py
+++ create_path_on_platform.py
@@ -5,6 +5,4 @@ def create_path_on_platform(path: str):
     if platform.system() != 'Windows' or '\\' in path:
         return path
     else:
-        if len(path) and path[0] == '.':
-            path = path.replace('.', '..', 1)
         return path.replace('/', '\\')
